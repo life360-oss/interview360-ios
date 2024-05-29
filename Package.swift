0be2e2cc13ffe9fc608501305abd2162180699ae
Kevin Christiano <kchristiano@life360.com>
2024-05-29 11:01:11 -0400

    init package

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "interview360-ios",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "interview360-ios",
+            targets: ["Aficionado"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Aficionado",
+            path: "Aficionado.xcframework")
+    ]
+)
